e534e6f55bd4a20790d81f8275682cfac645bb82
naaidrv <zabriverav@gmail.com>
2022-10-07 13:02:03 -0500

    Feat: Redesigning the project [Navbar - 1st View].

---
+++ js/myApp.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // Barra de navegación "pegada"
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // Animación
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // Efectoen la barra de Navegación
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
+
+    // Animación del textotipo maquina de escribir
+    var typed = new Typed(".typing", {
+        strings: [""],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
